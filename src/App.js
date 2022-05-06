663bed0d10c46c028bb0d928671438bbc622e69d
suthanhari <kharisuthan2000@gmail.com>
2022-05-06 12:07:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import ImageCropper from './component/GetCropped';
 
 function App() {
+
+  const [imageToCrop, setImageToCrop] = useState(undefined);
+  const [croppedImage, setCroppedImage] = useState(undefined);
+
+  const onUploadFile = (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const reader = new FileReader();
+
+      reader.addEventListener('load', () =>
+        setImageToCrop(reader.result)
+      );
+
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        
+        <div className='row'>
+          <div className='col-lg-6'>
+
+            <input
+              type="file"
+              accept="image/*"
+              onChange={onUploadFile}
+            />
+            <div>
+              <ImageCropper
+                imageToCrop={imageToCrop}
+                onImageCropped={(croppedImage) => setCroppedImage(croppedImage)}
+              />
+            </div>
+            {
+              croppedImage &&
+              <div>
+                <h2>Cropped Image</h2>
+                <img
+                  alt="Cropped Image"
+                  src={croppedImage}
+                />
+              </div>
+            }
+          </div>
+        </div>
+
+      </div>
+    </>
   );
 }
 
